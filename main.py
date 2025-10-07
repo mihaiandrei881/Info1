aaf60c9a824f1d8111c2e3060624b1cd9e82fd16
mihaiandrei881 <107351136+mihaiandrei881@users.noreply.github.com>
2025-10-07 09:26:38 +0300

    Pull 1

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+numer_secret = random.randint(1,10)
+
+
+ghicit = 0
+
+print ("Ghicheste numarul intre 1 si 10")
+
+while ghicit != numer_secret:
+    ghicit = int(input("Ghiceste numarul"))
+if ghicit < numer_secret:
+    print("Incearca un numar mai mare")
+if ghicit > numer_secret:
+    print("Incearca un numar mai mic")
+else:
+    print("L-ai ghicit!")
+
