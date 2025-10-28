2f2f1ccccdf0c7af858f1bf728d23d590def7746
mihaiandrei881 <107351136+mihaiandrei881@users.noreply.github.com>
2025-10-28 08:16:05 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+elevi = ["Ana", "Bogdan", "Carmen", "Darius", "Elena"]
+note  = [9,       7,        10,       4,        8]
+
+elev_nou        = "Felix"
+nota_elev_nou   = 6
+elev_de_sters   = "Darius"
+
+interogari_nume = ["Ana", "Mara", "Elena", "stop"]
+
+absente = [1, 0, 2, 3, 0]
+
+
+print ()
