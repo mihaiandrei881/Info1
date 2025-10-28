57c9c4c2ca51ad8c6aa4dcf9276f4f8afe851d59
mihaiandrei881 <107351136+mihaiandrei881@users.noreply.github.com>
2025-10-28 08:32:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,22 @@ interogari_nume = ["Ana", "Mara", "Elena", "stop"]
 absente = [1, 0, 2, 3, 0]
 
 
-print ()
+#A1
+for i in range(len(elevi)):
+    print(elevi[i], "are nota", note[i])
+
+#A2
+
+nota_maxima = max(note)
+nota_minima = min(note)
+
+pozitie_max = note.index(nota_maxima)
+pozitie_min = note.index(nota_minima)
+
+elev_max = elevi[pozitie_max]
+elev_min = elevi[pozitie_min]
+
+print("Elevul", elev_max, "are nota maxima", nota_maxima, "si Elevul", elev_min, "are nota minima", nota_minima)
+
+#A3
+
